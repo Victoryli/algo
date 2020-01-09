edde2c99f393d8d26bda024fac5e184272db72e6
liqing <1159838532@qq.com>
2020-01-09 13:59:09 +0800

    数据结构与算法（队列）

---
+++ src/java/queue/SingleArrayQueue.java
@@ -0,0 +1,47 @@
+package java.queue;
+
+/**
+ * @author liqing
+ * @version 1.0
+ * @date 2020-01-09 13:51
+ */
+public class SingleArrayQueue {
+
+    private int head = 0;
+    private int tail = 0;
+    private int length = 0;
+    private String[] data;
+
+    public SingleQueue() {
+
+    }
+
+    public void setQueueLength(int size) {
+        this.length = size;
+        this.data = new String[size];
+    }
+
+    public boolean inQueue(String e) {
+        if (this.tail == this.length) {
+
+            System.out.println("Queue is full");
+            return false;
+
+        }
+
+        this.data[this.tail] = e;
+        System.out.println("in: " + this.data[this.tail]);
+        tail++;
+        return true;
+    }
+
+    public String outQueue() {
+        if (this.head == this.tail)
+            return "false";
+        String out = this.data[head];
+        System.out.println("out: " + out);
+        head++;
+        return out;
+    }
+
+}
---
+++ src/java/queue/SingleArrayQueueOpt.java
@@ -0,0 +1,11 @@
+package java.queue;
+
+/**
+ * @author liqing
+ * @version 1.0
+ * @date 2020-01-09 13:55
+ */
+public class SingleArrayQueueOpt {
+
+
+}
