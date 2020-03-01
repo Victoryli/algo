39e1aa0bcf0fde8cadd3f5f4d81283954a8ab4a3
liqing <1159838532@qq.com>
2020-03-01 11:02:04 +0800

    归并排序(加注释)

--- src/com/sort/MergeSort.java
+++ src/com/sort/MergeSort.java
@@ -31,29 +31,33 @@ public class MergeSort {
 
     }
 
+    //数组合并
     public static void merge(int[] arr,int[] temp,int left,int mid,int right){
         int i = left;
         int j = mid+1;
         int t = 0;
 
         while (i <= mid && j <= right){
+            // 如果左边部分的值小于右边部分的，则将左边部分的值放入临时数组 temp 中
             if (arr[i] < arr[j]){
                 temp[t++] = arr[i++];
             }
-
+            // 如果右边部分的值小于左边部分的，则将右边部分的值放入临时数组 temp 中
             if (arr[j] < arr[i]){
                 temp[t++] = arr[j++];
             }
         }
 
+        //将左边部分剩余的元素放入临时数组 temp 中
         while (i <= mid){
             temp[t++] = arr[i++];
         }
-
+        //将右边部分剩余的元素放入临时数组 temp 中
         while (j <= right){
             temp[t++] = arr[j++];
         }
 
+        //将临时数组 temp 的值拷贝到 arr 数组中
         t = 0;
         while (left <= right){
             arr[left++] = temp[t++];
